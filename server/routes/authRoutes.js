721707d035701f0ad7b5665f15c7eeccb674d56a
SonaliV14 <sonalivarshney1404@gmail.com>
2025-08-28 22:25:45 +0530

    Added a JWT middleware for secured authentication at login/signup

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,10 +1,16 @@
 import express from "express";
 import { signup, login, googleLogin } from "../controllers/authController.js";
+import authMiddleware from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
 router.post("/signup", signup);
 router.post("/login", login);
 router.post("/google", googleLogin);
+
+// Protected route (only accessible with valid JWT)
+router.get("/profile", authMiddleware, (req, res) => {
+  res.json({ message: "Profile data", user: req.user });
+});
 
 export default router;
